a1bf06a3eb9d4c138a2b519be3b28d801dee0ead
Thibault CHANUT <t.chanut@game-sup.fr>
2026-01-16 14:39:33 +0100

    add enum

--- test_GitHub/Test_GitHub/Assets/Scripts/S_HelloWorld.cs
+++ test_GitHub/Test_GitHub/Assets/Scripts/S_HelloWorld.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 public class S_HelloWorld : MonoBehaviour
 {
     [SerializeField] private int _numberOfTick = 0;
+    private enum State
+    {
+        M,
+        Mi,
+        Mia,
+        Miao,
+        Miaou
+    }
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,5 +25,6 @@ public class S_HelloWorld : MonoBehaviour
     {
         _numberOfTick++;
         Debug.Log(_numberOfTick);
+        Debug.Log(State.Miao);
     }
 }
