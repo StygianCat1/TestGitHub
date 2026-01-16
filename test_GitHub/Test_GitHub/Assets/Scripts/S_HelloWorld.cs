3786d90bec82056e921cb1978f61b5ce227f0f55
FeronB <b.feron@game-sup.fr>
2026-01-16 14:19:55 +0100

    Create Chaos
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠴⠖⠒⠚⠛⠛⠒⠚⠃⠻⠶⠶⣶⣶⣤⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⠤⠖⡛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⢿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠹⣄⣠⡴⠋⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⢀⡿⠃⠀⠀⠀⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⣷⡄⠀⠀⠀⠀⠙⢿⣿⣿⣦⡀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⣠⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⢠⠋⠀⠀⠀⢀⠞⠀⢳⠀⠀⠀⠀⠀⠀⠙⢿⣿⣷⣄⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⣰⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠃⠐⠁⠀⠀⠀⠀⠋⣠⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣆⠀⠀⠀
    ⠀⠀⠀⠀⢰⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊⠀⡄⠀⠀⠀⠀⠀⣠⠞⠁⢻⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⡆⠀⠀
    ⠀⠀⠀⢀⡏⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⠇⠀⠠⢂⣠⠞⠁⠀⠀⠈⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡀⠀
    ⠀⠀⠀⡾⢀⡀⠀⢰⠋⠁⠀⠀⣀⣀⣀⣤⣴⣶⠾⠟⢋⣿⢀⣠⠴⠛⠁⠀⠀⠀⠀⠀⠸⣿⡄⠀⠀⠀⠀⠀⠀⠀⡀⠘⣿⣿⣧⠀
    ⠀⢴⣾⡟⢛⣇⣠⣯⣀⣤⣴⣿⠟⠛⠋⠉⠁⠀⠀⠀⠚⣋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⢀⠀⠀⠀⠀⠀⡜⠀⠀⣿⣿⣿⠀
    ⠀⠸⡏⠉⠉⣿⣿⢁⣾⣿⣿⣿⣷⠀⠀⠀⠀⠀⠐⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣦⡀⠈⢿⡆⠀⠀⠀⡼⠁⠀⣠⣿⣿⣿⠆
    ⠀⠀⢳⠀⣠⡿⠁⠸⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⡿⠀⠀⣧⠀⠀⢠⣁⣠⡬⠵⠿⠿⠿⡦
    ⠀⠀⢈⣿⠟⡵⠀⠀⠈⠙⠛⠋⠁⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⣿⣿⡿⠃⠀⠀⣿⠀⠀⡾⠉⠀⠀⠀⠀⢐⡼⠁
    ⠀⣴⡿⠋⡜⠁⠀⠀⠀⠀⠀⠀⢰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⢰⡇⠀⠀⠀⠀⣠⡾⠁⠀
    ⠸⣿⡀⠘⡀⠀⠀⠀⠀⠀⠀⠀⠘⣷⣀⣀⣤⣾⣦⡀⠀⠀⠀⠀⣀⣴⠇⠀⠀⠀⠀⠀⠀⠀⢠⣿⠃⠀⣼⠁⠀⠀⢀⣴⣿⠁⠀⠀
    ⠀⢻⣧⡀⠣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠈⠛⠛⠛⠛⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⢠⢫⡟⠀⠀⡟⠀⢀⣴⣿⣿⡟⠀⠀⠀
    ⠀⠀⠙⢿⣄⣠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢎⡿⠀⢠⠀⢿⣾⣿⣿⣿⣿⡇⠀⠀⠀
    ⠀⠀⠀⠀⠙⢿⣿⣷⣂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠋⣸⠁⠀⠇⡀⢸⣿⣿⣿⣿⣿⡇⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠹⣍⠛⢻⡷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⡾⠿⢿⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠙⣦⡼⠁⠀⠀⠀⠉⢉⣩⡏⠀⠀⠀⠀⠒⣖⠒⠛⠛⠛⠋⠉⠀⠀⠀⠘⡇⢀⣼⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⣠⣶⡟⠉⠛⠒⠶⠶⠦⠶⢿⣻⣦⣄⡀⠀⠀⠀⠀⠀⠀⠷⠻⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⢿⣿⡀⠀⠀⢀⣠⣤⣶⠿⣛⣁⠍⣻⣦⣀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⢷⡿⠁⠐⣿⣷⣶⣾⣿⠿⠀⠁⠀⠁⠀⣼⠟⣡⡿⣧⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠁⣼⠇⠀⠀⠘⣷⡾⠋⠁⠀⠀⠀⠀⢀⠰⣿⡟⠉⠀⠈⣧⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⠃⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⢀⡿⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠘⣰⡏⠀⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⠀⠈⠻⠟⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⠀⢸⣧⣼⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣼⣇⣀⣀⣀⣀⣠⣷⠀⠀⠀

--- test_GitHub/Test_GitHub/Assets/Scripts/S_HelloWorld.cs
+++ test_GitHub/Test_GitHub/Assets/Scripts/S_HelloWorld.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class S_HelloWorld : MonoBehaviour
 {
+    private float A = 10 / 3;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Miaou");
+        Debug.Log (A);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Miaou");
+        print("mIAOU");
     }
 }
